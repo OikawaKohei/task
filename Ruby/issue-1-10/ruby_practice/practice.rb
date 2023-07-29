189a7e914d2a50c17e65d638843652ee5f08ff7d
OikawaKohei <k.oikawa19930113@icloud.com>
2023-07-29 08:47:56 +0000

    1-10課題　見やすくしました

--- Ruby/issue-1-10/ruby_practice/practice.rb
+++ Ruby/issue-1-10/ruby_practice/practice.rb
@@ -1,21 +1,37 @@
+# 練習
+puts '==practice=='
 puts 'hello ruby!'
+puts '============'
 
+# 課題1
 a = 3
 b = 7
+puts '===issue1==='
 puts a + b
+puts '============'
 
+#　課題２"
 array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"]
+puts '===issue2==='
 puts array_month[7]
+puts '============'
 
+# 課題３
 hello = "Hello,"
 name = "Kohei"
 world = "'s World"
+puts '===issue3==='
 puts hello + name + world
+puts '============'
 
+# 課題４
 omusubi_tech = 'omusubi'
 omusubi_tech << ' tech'
+puts '===issue4==='
 puts omusubi_tech
+puts '============'
 
+# 課題5
 calendar_2023 = {
   "January" => "1月",
   "February" => "2月",
@@ -30,6 +46,7 @@ calendar_2023 = {
   "November" => "11月",
   "December" => "12月"
 }
+puts '===issue5==='
 puts calendar_2023["December"]
-
+puts '============'
 
